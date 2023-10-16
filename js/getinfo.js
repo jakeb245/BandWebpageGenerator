9eb942a6b8275bde39ac0961cbd129237eb31bce
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-10-16 13:05:56 -0400

    Access tokens work

---
+++ js/getinfo.js
@@ -0,0 +1,4 @@
+function getInput() {
+    let artist = document.getElementById("artist").value
+    document.getElementById("band_name").innerHTML = artist;
+}
---
+++ js/testing.js
@@ -0,0 +1,7 @@
+
+function testTime() {
+    const dur = 3600;
+    const exp = Date.now() + (dur*1000);
+    console.log(Date.now());
+    console.log(exp);
+}
