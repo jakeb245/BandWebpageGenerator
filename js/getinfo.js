79e0fad8f658862158e5f4c437d6ad218785a3da
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-10-16 13:41:21 -0400

    Starting to search

--- js/getinfo.js
+++ js/getinfo.js
@@ -1,4 +1,24 @@
-function getInput() {
-    let artist = document.getElementById("artist").value
+
+
+async function getArtistInfo(artist) {
+
+    let url = `https://api.spotify.com/v1/search?type=artist&q=${artist}&limit=1`
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            'Authorization' : `Bearer ${localStorage.getItem("token")}`
+        }
+    });
+    return response.json();
+}
+
+function showArtistInfo() {
+    // run on 'search' button click
+    const artist = document.getElementById('artist').value;
+    console.log(artist);
     document.getElementById("band_name").innerHTML = artist;
+    getArtistInfo(artist).then((data) => {
+        console.log(data);
+    });
+    //document.getElementById('artist_image').src = img;
 }
