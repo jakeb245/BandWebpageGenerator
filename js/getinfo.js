9b7b3e95e097ad4b4a16be2629c3b3610d2b1ba4
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-10-18 11:21:49 -0400

    Move Spotify link to band name header

--- js/getinfo.js
+++ js/getinfo.js
@@ -1,8 +1,27 @@
 
+let artistInfo;
 
 async function getArtistInfo(artist) {
+    // FIXME: Limit this to ~3 and pick whichever matches the query best
+    // Otherwise, typing "Rainbow" gives "Rainbow Kitten Surprise"...
+    const url = `https://api.spotify.com/v1/search?type=artist&q=${artist}&limit=1`
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            'Authorization' : `Bearer ${localStorage.getItem("token")}`
+        }
+    });
+    return response.json();
+}
 
-    let url = `https://api.spotify.com/v1/search?type=artist&q=${artist}&limit=1`
+function toSpotifyPage() {
+    const url = artistInfo.external_urls['spotify'];
+    window.open(url);
+}
+
+async function getAlbums(artist_id) {
+    // FIXME: fix case where number of albums is greater than the default limit (20)
+    const url = `https://api.spotify.com/v1/artists/${artist_id}/albums?include_groups=single,album,compilation`
     const response = await fetch(url, {
         method: "GET",
         headers: {
@@ -15,17 +34,20 @@ async function getArtistInfo(artist) {
 async function showArtistInfo() {
     // run on 'search' button click
     const artist = document.getElementById('artist').value;
-    const artistInfo = await getArtistInfo(artist).then((data) => {
+    artistInfo = await getArtistInfo(artist).then((data) => {
         console.log(data.artists.items[0]);
         return data.artists.items[0];
     });
     // Band name
     document.getElementById("band_name").innerHTML = artistInfo.name;
+    document.getElementById("band_name").href = artistInfo.external_urls['spotify'];
     // Photo
     document.getElementById("photo").src = artistInfo.images[0].url;
     document.getElementById("photo").width = 300;
     document.getElementById("photo").height = 300;
-    // Link to Spotify page
-    document.getElementById("spotify_link").innerHTML = `View ${artistInfo.name} on Spotify`;
-    document.getElementById("spotify_link").href = artistInfo.external_urls['spotify'];
+    const albums = await getAlbums(artistInfo.id).then((data) => {
+        console.log(data);
+        return data.items;
+    });
 }
+
